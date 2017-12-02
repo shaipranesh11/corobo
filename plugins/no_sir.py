20f686ca00ad8fa6a9e3c8b00c70a708cbd93f3c
shaipranesh11 <34070893+shaipranesh11@users.noreply.github.com>
2017-12-02 15:30:00 +0530

    creating no_sir.py: to tell users to not use 'sir'
    Ask people to not use 'sir' when they do #337.

---
+++ plugins/no_sir.py
@@ -0,0 +1,13 @@
+from errbot import BotPlugin
+class no_sir(BotPlugin):
+    """do not use sir to sound more friendly"""  # Ignore QuotesBear
+
+    def callback_message(self, msg):
+        emots = [':D']
+        if match_sir:
+            self.send(
+                msg.frm,
+                '@{}, donot use \'sir\' in ur conversation. {}'.format(
+                    msg.frm.nick, emots[0]
+                )
+            )
